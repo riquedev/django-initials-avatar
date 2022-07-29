75e2238b81067cf05c30baaa689b32525eb03b04
riquedev <rique_dev@hotmail.com>
2022-07-29 14:37:04 -0300

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="django_initials_avatar",
-    version="0.0.1",
+    version="0.0.2",
     author="Henrique da Silva Santos",
     author_email="henrique.santos@4u360.com.br",
     description="A ridiculously simple avatar generator with initials from names.",
